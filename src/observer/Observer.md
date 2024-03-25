b5020f4e2fb012a5b7d7000e9f1ed1725ee8c283
choomi1217 <whdudal1217@naver.com>
2024-03-26 01:51:39 +0900

    :sparkles:

---
+++ src/observer/Observer.md
@@ -0,0 +1,5 @@
+# 옵저버 패턴
+
+
+### 예제코드
+> WeatherData라는 주제(Subject)를 구독하는 Displays
---
+++ src/observer/weather/displays/CurrentConditionsDisplay.java
@@ -0,0 +1,29 @@
+package observer.weather.displays;
+
+import observer.weather.DisplayElement;
+import observer.weather.Observer;
+import observer.weather.WeatherData;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private WeatherData weatherData;
+
+    public CurrentConditionsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: " + temperature +
+            "F degrees and " + humidity + "% humidity");
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        this.temperature = temp;
+        this.humidity = humidity;
+        display();
+    }
+}
